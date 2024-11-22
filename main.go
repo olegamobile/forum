070573e387e9482c817f6e83dc8cb823803b5da9
Markus Helsinki <markus.amberla@gritlab.ax>
2024-11-22 08:55:55 +0200

    replies to replies working with deep levels

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ type Thread struct {
 	Dislikes   int
 	RepliesN   int
 	Replies    []Reply
+	BaseID     int
 }
 
 type Reply struct {
@@ -38,6 +39,7 @@ type Reply struct {
 	ParentID   int
 	ParentType string
 	Replies    []Reply
+	BaseID     int
 }
 
 type PageData struct {
@@ -48,7 +50,7 @@ func makeTables(db *sql.DB) {
 	// Create threads table if it doesn't exist
 	createThreadsTableQuery := `
 	CREATE TABLE IF NOT EXISTS threads (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		id INTEGER PRIMARY KEY AUTOINCREMENT,		
 		author TEXT NOT NULL,
 		title TEXT NOT NULL,
 		content TEXT NOT NULL,
@@ -66,6 +68,7 @@ func makeTables(db *sql.DB) {
 	createRepliesTableQuery := `
 		CREATE TABLE IF NOT EXISTS replies (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			base_id INTEGER DEFAULT 0,
 			author TEXT NOT NULL,
 			content TEXT NOT NULL,
 			created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -92,19 +95,20 @@ func fetchThreads(db *sql.DB) ([]Thread, error) {
 	var threads []Thread
 	for rows.Next() {
 		var th Thread
-
 		err := rows.Scan(&th.ID, &th.Author, &th.Title, &th.Content, &th.Created, &th.Categories, &th.Likes, &th.Dislikes)
 		if err != nil {
 			fmt.Println("fetchThreads rows scanning:", err.Error())
 			return nil, err
 		}
+		th.BaseID = th.ID
 		threads = append(threads, th)
 	}
+
 	return threads, nil
 }
 
 func fetchReplies(db *sql.DB) ([]Reply, error) {
-	selectQuery := `SELECT id, author, content, created_at, likes, dislikes, parent_id, parent_type FROM replies;`
+	selectQuery := `SELECT id, base_id, author, content, created_at, likes, dislikes, parent_id, parent_type FROM replies;`
 	rows, err := db.Query(selectQuery)
 	if err != nil {
 		return nil, err
@@ -114,7 +118,7 @@ func fetchReplies(db *sql.DB) ([]Reply, error) {
 	var replies []Reply
 	for rows.Next() {
 		var re Reply
-		err := rows.Scan(&re.ID, &re.Author, &re.Content, &re.Created, &re.Likes, &re.Dislikes, &re.ParentID, &re.ParentType)
+		err := rows.Scan(&re.ID, &re.BaseID, &re.Author, &re.Content, &re.Created, &re.Likes, &re.Dislikes, &re.ParentID, &re.ParentType)
 		if err != nil {
 			return nil, err
 		}
@@ -154,17 +158,49 @@ func addReplyHandler(db *sql.DB, w http.ResponseWriter, r *http.Request, parentT
 	if r.Method == http.MethodPost {
 		author := "Auteur"
 		content := strings.TrimSpace(r.FormValue("content"))
-		parId := r.FormValue("parentId")
+		parId := r.FormValue("parentId") // No int conversion necessary
+		baseId := r.FormValue("baseId")  // No int conversion necessary
 
 		if content != "" {
-			_, err := db.Exec(`INSERT INTO replies (author, content, parent_id, parent_type) VALUES (?, ?, ?, ?);`, author, content, parId, parentType)
+			_, err := db.Exec(`INSERT INTO replies (base_id, author, content, parent_id, parent_type) VALUES (?, ?, ?, ?, ?);`, baseId, author, content, parId, parentType)
 			if err != nil {
 				fmt.Println("Replying:", err.Error())
 				http.Error(w, "Error adding reply", http.StatusInternalServerError)
 				return
 			}
 		}
-		http.Redirect(w, r, "/thread/"+r.FormValue("baseId"), http.StatusSeeOther)
+		http.Redirect(w, r, "/thread/"+baseId, http.StatusSeeOther)
+	}
+}
+
+func recurseReplies(db *sql.DB, this *Reply) {
+	selectQueryReplies := `SELECT id, base_id, author, content, created_at, likes, dislikes FROM replies WHERE parent_id = ? AND parent_type = ?;`
+	rows, err := db.Query(selectQueryReplies, this.ID, "reply")
+	if err != nil {
+		fmt.Println("Error getting replies for reply:", err.Error())
+		return
+	}
+	defer rows.Close()
+
+	var replies []Reply
+	for rows.Next() {
+		var re Reply
+		err2 := rows.Scan(&re.ID, &re.BaseID, &re.Author, &re.Content, &re.Created, &re.Likes, &re.Dislikes)
+		if err2 != nil {
+			fmt.Println("Error rading reply rows for reply:", err2.Error())
+			return
+		}
+		re.ParentID = this.ID
+		re.ParentType = "reply"
+		replies = append(replies, re)
+	}
+
+	if len(replies) != 0 {
+
+		this.Replies = replies
+		for i := 0; i < len(this.Replies); i++ {
+			recurseReplies(db, &this.Replies[i])
+		}
 	}
 }
 
@@ -173,7 +209,7 @@ func findThread(db *sql.DB, id int) (Thread, error) {
 	selectQueryThread := `SELECT id, author, title, content, created_at, categories, likes, dislikes FROM threads WHERE id = ?;`
 	err1 := db.QueryRow(selectQueryThread, id).Scan(&thread.ID, &thread.Author, &thread.Title, &thread.Content, &thread.Created, &thread.Categories, &thread.Likes, &thread.Dislikes)
 
-	selectQueryReplies := `SELECT id, author, content, created_at, likes, dislikes FROM replies WHERE parent_id = ? AND parent_type = ?;`
+	selectQueryReplies := `SELECT id, base_id, author, content, created_at, likes, dislikes FROM replies WHERE parent_id = ? AND parent_type = ?;`
 	rows, err2 := db.Query(selectQueryReplies, thread.ID, "thread")
 	if err2 != nil {
 		return thread, err2
@@ -183,14 +219,22 @@ func findThread(db *sql.DB, id int) (Thread, error) {
 	var replies []Reply
 	for rows.Next() {
 		var re Reply
-		err3 := rows.Scan(&re.ID, &re.Author, &re.Content, &re.Created, &re.Likes, &re.Dislikes)
+		err3 := rows.Scan(&re.ID, &re.BaseID, &re.Author, &re.Content, &re.Created, &re.Likes, &re.Dislikes)
 		if err3 != nil {
 			return thread, err3
 		}
+		re.ParentID = thread.ID
+		re.ParentType = "thread"
 		replies = append(replies, re)
 	}
-	thread.Replies = replies
 
+	// Add replies to replies recursively
+	for i := 0; i < len(replies); i++ {
+		recurseReplies(db, &(replies[i]))
+	}
+
+	thread.Replies = replies
+	thread.BaseID = thread.ID
 	return thread, err1
 }
 
@@ -205,6 +249,7 @@ func threadPageHandler(db *sql.DB, tmpl *template.Template, w http.ResponseWrite
 
 	thread, err := findThread(db, threadID)
 	if err != nil {
+		fmt.Println("Find thread error:", err.Error())
 		http.Error(w, "Thread not found", http.StatusNotFound)
 		return
 	}
@@ -267,7 +312,7 @@ func main() {
 		fmt.Println("Error parsing template:", err)
 		return
 	}
-	threadTmpl, err := template.ParseFiles("static/thread.html")
+	threadTmpl, err := template.ParseFiles("static/thread.html", "static/reply.html")
 	if err != nil {
 		fmt.Println("Error parsing template:", err)
 		return
