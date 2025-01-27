02d38cdf55913ff4b1250582ed9e213bc37b93be
Oleg Balandin <oleg.balandin@gritlab.ax>
2025-01-27 21:32:27 +0200

    Category selector added to filter

--- static/categories.js
+++ static/categories.js
@@ -1,7 +1,7 @@
-function updateCategory() {
-    const select = document.getElementById('categorySelector');
+function updateCategory(fieldName, selectorName) {
+    const select = document.getElementById(selectorName);
     const selectedCategory = select.value;
-    const categoriesInput = document.getElementById('categories');
+    const categoriesInput = document.getElementById(fieldName);
     if (!categoriesInput.value.includes(selectedCategory)) {
         categoriesInput.value += selectedCategory + ' ';
       }
